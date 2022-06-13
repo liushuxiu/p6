3d3df1e1095dddc5574442723307be9de344d739
天泽履 <917733234@qq.com>
2022-06-13 22:32:11 +0800

    start

---
+++ src/main/java/com/uestc/P6Application.java
@@ -0,0 +1,13 @@
+package com.uestc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/uestc/P6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uestc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
